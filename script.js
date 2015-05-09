e50dd8b334f47946d8f7d5a119bbf986bb58ec3e
Roman Berla <rberla@ya.ru>
2015-05-09 15:12:30 +0300

    Something

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $('div.wrapper').on('click', 'div.gold a.calc', function() {
 		result = input * 1 + 6.88;
 		
 		if(result <= 20) {
-			$('div.silver div.result').css({'background-color': 'rgba(140, 140, 140, 0.3)'});
+			$('div.silver div.result').css({'background-color': 'rgba(190, 190, 190, 0.3)'});
 			$('div.gold div.result p').text(result.toFixed(1));
 			$('div.gold p.comment').text('На такий бал має бути ваш твір');
 		} else {
@@ -51,7 +51,7 @@ $('div.wrapper').on('click', 'div.silver a.calc', function() {
 		result = input * 1 + 1.65;
 		
 		if(result <= 20) {
-			$('div.silver div.result').css({'background-color': 'rgba(140, 140, 140, 0.3)'});
+			$('div.silver div.result').css({'background-color': 'rgba(190, 190, 190, 0.3)'});
 			$('div.silver div.result p').text(result.toFixed(1));
 			$('div.silver p.comment').text('На такий бал має бути ваш твір');
 		} else {
