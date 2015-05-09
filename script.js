742c4de0e48593b5195d523660ab70800219a913
Roman Berla <rberla@ya.ru>
2015-05-09 13:08:49 +0300

    Silver medal added

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ $('div.wrapper').on('click', 'input', function() {
 		'margin-top': '0'
 	});
 });
-$('div.wrapper').on('click', 'a.calc', function() {
+
+$('div.wrapper').on('click', 'div.gold a.calc', function() {
 	var input = $('div.input-block input').val();
 	var result;
 	if(input != '' && input * 0 != 'NaN' && input >= 0) {
@@ -14,15 +15,15 @@ $('div.wrapper').on('click', 'a.calc', function() {
 		result = input * 1 + 6.87;
 		
 		if(result <= 20) {
-			$('div.result p').text(result.toFixed(1));
-			$('p.comment').text('На такий бал має бути ваш твір');
+			$('div.gold div.result p').text(result.toFixed(1));
+			$('div.gold p.comment').text('На такий бал має бути ваш твір');
 		} else {
-			$('div.result').css({'background-color': 'red'});
-			$('div.result p').text('༼ ºل͟º ༽');
-			$('p.comment').text('Медалі не буде');
+			$('div.gold div.result').css({'background-color': 'red'});
+			$('div.gold div.result p').text('༼ ºل͟º ༽');
+			$('div.gold p.comment').text('Медалі не буде');
 		}
 		
-		$('div.result img').css({
+		$('div.gold div.result img').css({
 			'margin-top': '-250px',
 			'-webkit-animation': 'movingUp 7s',
 			'-moz-animation': 'movingUp 7s',
@@ -30,7 +31,7 @@ $('div.wrapper').on('click', 'a.calc', function() {
 			'animation': 'movingUp 7s'
 		});
 		
-		$('p.comment').css({
+		$('div.gold p.comment').css({
 			'opacity': '1'
 		});
 		
@@ -38,6 +39,42 @@ $('div.wrapper').on('click', 'a.calc', function() {
 		$('div.input-block input').addClass('error');
 	}
 });
+
+$('div.wrapper').on('click', 'div.silver a.calc', function() {
+	var input = $('div.input-block input').val();
+	var result;
+	if(input != '' && input * 0 != 'NaN' && input >= 0) {
+		
+		$('div.input-block input').removeClass('error');
+		
+		result = input * 1 + 1.65;
+		
+		if(result <= 20) {
+			$('div.silver div.result p').text(result.toFixed(1));
+			$('div.silver p.comment').text('На такий бал має бути ваш твір');
+		} else {
+			$('div.silver div.result').css({'background-color': 'red'});
+			$('div.silver div.result p').text('༼ ºل͟º ༽');
+			$('div.silver p.comment').text('Медалі не буде');
+		}
+		
+		$('div.silver div.result img').css({
+			'margin-top': '-250px',
+			'-webkit-animation': 'movingUp 7s',
+			'-moz-animation': 'movingUp 7s',
+			'-0-animation': 'movingUp 7s',
+			'animation': 'movingUp 7s'
+		});
+		
+		$('div.silver p.comment').css({
+			'opacity': '1'
+		});
+		
+	} else {
+		$('div.input-block input').addClass('error');
+	}
+});
+
 $(document).ready(function() {
 	$('header').text("Чи матиму медаль?");
 	$('div.input-block div p').text('Ваша кількість помилок у тестах з мови (БЕЗ КОМАХ)');
